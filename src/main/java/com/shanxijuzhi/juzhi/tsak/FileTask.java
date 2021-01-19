495bf7c2b39e6138df6be3cacecac5b0d40977a4
Zhazhahui8954 <895486189@qq.com>
2021-01-19 17:26:45 +0800

    fffff    分支本地

--- src/main/java/com/shanxijuzhi/juzhi/tsak/FileTask.java
+++ src/main/java/com/shanxijuzhi/juzhi/tsak/FileTask.java
@@ -22,6 +22,12 @@ public class FileTask<T> {
     @Scheduled(cron="0/15 * * * * *")
     public List<TestDataInfo> scanFile() throws Exception {
 
+
+        System.out.println("woshi本地dev提交的数据");
+        System.out.println("我是本地master 提交的数据");
+        System.out.println("fffff    分支本地");
+        System.out.println("我又是新增加的远程   master_dev分支");
+        System.out.println("我又是新增加的远程   master_dev分支");
         System.out.println("fffff    分支本地");
         System.out.println("我是dev ------------- dev  dev     ");
         System.out.println("开始进入文件扫描的程序..................");
