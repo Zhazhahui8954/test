24392c35f4cad73b32309a1f3989f9b58dc796e6
Zhazhahui8954 <895486189@qq.com>
2021-01-19 14:24:44 +0800

    不是dev提交

--- src/main/java/com/shanxijuzhi/juzhi/tsak/FileTask.java
+++ src/main/java/com/shanxijuzhi/juzhi/tsak/FileTask.java
@@ -54,9 +54,7 @@ public class FileTask<T> {
 
                     System.out.println(tt);
                 }*/
-                System.out.println("1111111111111111111");
-                System.out.println("1111111111111111111");
-                System.out.println("1111111111111111111");
+                System.out.println("woshi新加的分支。。。不是dev");
                 System.out.println("1111111111111111111");
                 System.out.println("1111111111111111111");
 
