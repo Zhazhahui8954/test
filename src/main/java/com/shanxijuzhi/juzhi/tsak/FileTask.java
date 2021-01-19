fff4dbdf76abf0bf0c2fd434d0b9b8ad9c6db3f7
Zhazhahui8954 <895486189@qq.com>
2021-01-19 14:42:54 +0800

    我是提交的dev dev

--- src/main/java/com/shanxijuzhi/juzhi/tsak/FileTask.java
+++ src/main/java/com/shanxijuzhi/juzhi/tsak/FileTask.java
@@ -22,6 +22,7 @@ public class FileTask<T> {
     @Scheduled(cron="0/15 * * * * *")
     public List<TestDataInfo> scanFile() throws Exception {
 
+        System.out.println("我是dev ------------- dev  dev     ");
         System.out.println("开始进入文件扫描的程序..................");
         String saveUrl = url;
         System.out.println("存储文件的地址为"+ saveUrl);
